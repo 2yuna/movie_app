466550a46aea5991da8f74ff4e38042bd1b7a623
이유나 <yuna@iyunaui-MacBookPro.local>
2021-03-14 22:38:02 +0900

    #4.0 Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
     isLoading: true,
   };
+
+  getMovies = async () => {
+    const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    console.log(movies);
+  };
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
   render() {
     const { isLoading } = this.state;
